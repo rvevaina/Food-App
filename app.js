3fc5f9a3c1fe163ce7275b091a0d5de6595f4ae4
rayhan_v@me.com <rayhan_v@me.com>
2014-05-24 01:19:38 -0400

    Listings added

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var foodApp = {}
 
 //Make a property to hold API key
 
-foodApp.apiKey = "09c4850c082c00bab47f2116a134724e";
-foodApp.appId = "55ce3b8a";
+// foodApp.apiKey = "wgjnrqfprz74ue73gh7ksnhb";
+// foodApp.UID = "Eat+This-Drink+that"
+
 // method to make AJAX request inside init function
 
 foodApp.init= function(){
@@ -19,19 +20,16 @@ foodApp.getlisting ();
 foodApp.getlisting=function(query){
 	var userChoice = "burger + beer"
 	$.ajax({
-  	url: 'https://api.nutritionix.com/v1_1/search/' + userChoice,
+  	url: 'http://api.sandbox.yellowapi.com/FindBusiness/?what=nachos+tequila&where=Downtown+Toronto&pgLen=400&pg=1&dist=1&fmt=JSON&lang=en&UID=Eat+This-Drink+that&apikey=wgjnrqfprz74ue73gh7ksnhb',
   	type: 'GET',
 	  data: {
-	  appKey: foodApp.apiKey,
-	  appId: foodApp.appId,
 	  format:'jsonp',
-	  cal_min: '0',
-	  cal_max: '50000'
 	  },
 	  success: function(result) {
 	  	 // console.log(result);
-	  	 foodApp.displaylisting(result.brand_name);
-	  	 console.log(brand_name)
+	  	 foodApp.displaylisting(result.listings);
+	  	 // foodApp.displaylisting1(result.listings.address)
+	  	 
 	  }
 	});
 };	
@@ -42,23 +40,48 @@ $( ".button" ).click(function(){
 	foodApp.init();
 });
 
-// parseData = function(data){
-//     //put art stuff on the page
-//   $.each(data, function(i, piece){
+//Create a new displayPieces method that will handle parsing 
+
+foodApp.displaylisting = function(data){
+//     //put restaurant listings on the page
+  $.each(data, function(i, chunk){
 //   	//Inside the $.each function generate the HTML required
-//     var title = $('<h2>').text(piece.title);
+    var title = $('<h2>').text(chunk.name);
+    var address =$('<p>').text(chunk.address.street);
+    var distance =$('<p>').text(chunk.distance);
+    var listings = $('<div>').addClass('restaurant').append(title, distance, address);
+    // var location = $('<div>').addClass('address').append(address)
+// foodApp.
 // 	var artist = $('<p>').addClass('artist').text(piece.principalOrFirstMaker);
 // 	var image = $('<img>').attr('src', piece.webImage.url);
 // 	var artPiece = $('<div>').addClass('piece').append(image, title, artist);
 // 	//Finally, we append the completed artPiece divs into the DOM.
-// 	$('#artwork').append(artPiece);
+	$('#restaurants').append(listings);
+	
+
+   });
+};
+
+// foodApp.displaylisting1 = function(data){
+// //     //put restaurant listings on the page
+//   $.each(data, function(i, chunk){
+//  // var title = $('<h2>').text(chunk.name);
+//     var street =$('<p>').text(chunk.street);
+//     // var distance =$('<p>').text(chunk.distance);
+//     // var listings = $('<div>').addClass('restaurant').append(title, distance);
+//     var location = $('<div>').addClass('address').append(street)
+// // foodApp.
+// // 	var artist = $('<p>').addClass('artist').text(piece.principalOrFirstMaker);
+// // 	var image = $('<img>').attr('src', piece.webImage.url);
+// // 	var artPiece = $('<div>').addClass('piece').append(image, title, artist);
+// // 	//Finally, we append the completed artPiece divs into the DOM.
+// 	$('#restaurantAdd').append(location);
+
 //    });
 // };
 
-
-
 //Start of JQuery to get the JS to actually run ****************************************
 
-$(function(){
-  foodApp.init();
-}); 
+// $(function(){
+//   foodApp.init();
+// }); 
