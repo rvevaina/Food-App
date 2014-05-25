99a111635979be1e4ae3637278448bde974c3e92
rayhan_v@me.com <rayhan_v@me.com>
2014-05-24 21:45:26 -0400

    Image changes

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ foodApp.getlisting ();
 //Ajax request 
 
 foodApp.getlisting=function(query){
-	var userChoice = "burger + beer"
 	$.ajax({
   	url: 'http://api.sandbox.yellowapi.com/FindBusiness/?what=nachos+tequila&where=Downtown+Toronto&pgLen=400&pg=1&dist=1&fmt=JSON&lang=en&UID=Eat+This-Drink+that&apikey=wgjnrqfprz74ue73gh7ksnhb',
   	type: 'GET',
@@ -48,40 +47,54 @@ foodApp.displaylisting = function(data){
 //   	//Inside the $.each function generate the HTML required
     var title = $('<h2>').text(chunk.name);
     var address =$('<p>').text(chunk.address.street);
-    var distance =$('<p>').text(chunk.distance);
-    var listings = $('<div>').addClass('restaurant').append(title, distance, address);
+    var address1 =$('<p>').text(chunk.address.city);
+    var address2 =$('<p>').text(chunk.address.prov);
+    var address3 =$('<p>').text(chunk.address.pcode);
+    var distance =$('<p>').text(chunk.distance + " km from Downtown Core");
+    var listings = $('<div>').addClass('restaurant').append(title, distance, address, address1,address2,address3);
     // var location = $('<div>').addClass('address').append(address)
 // foodApp.
-// 	var artist = $('<p>').addClass('artist').text(piece.principalOrFirstMaker);
-// 	var image = $('<img>').attr('src', piece.webImage.url);
-// 	var artPiece = $('<div>').addClass('piece').append(image, title, artist);
-// 	//Finally, we append the completed artPiece divs into the DOM.
-	$('#restaurants').append(listings);
-	
 
+// 	//Finally, we append the completed listings divs into the DOM.
+	$('#restaurants').append(listings);
    });
 };
 
-// foodApp.displaylisting1 = function(data){
-// //     //put restaurant listings on the page
-//   $.each(data, function(i, chunk){
-//  // var title = $('<h2>').text(chunk.name);
-//     var street =$('<p>').text(chunk.street);
-//     // var distance =$('<p>').text(chunk.distance);
-//     // var listings = $('<div>').addClass('restaurant').append(title, distance);
-//     var location = $('<div>').addClass('address').append(street)
-// // foodApp.
-// // 	var artist = $('<p>').addClass('artist').text(piece.principalOrFirstMaker);
-// // 	var image = $('<img>').attr('src', piece.webImage.url);
-// // 	var artPiece = $('<div>').addClass('piece').append(image, title, artist);
-// // 	//Finally, we append the completed artPiece divs into the DOM.
-// 	$('#restaurantAdd').append(location);
+//define pairs function 
+	function testmatch() {
+      var foodOne = pairs[0] + pairs[1];
+      var foodTwo = pairs[2] + pairs[3];
+      // var combine = foodOne + foodTwo;
+      // console.log(combine)
+	}
 
-//    });
-// };
+
+  $(".button1").click(function(){
+	
+// 		function changeImg(){ 
+// 	var imag = document.getElementsByTagName('img'); 
+// 	for(var i=0;i<imag.length;i++){ 
+// 	if(imag[i].getAttribute('src')=='images/BEER.jpg'){ 
+// 	imag[i].removeAttribute('height'); 
+// 	imag[i].setAttribute('src','images/bourbon.png'); 
+// } 
+// } 
+// } 
+
+	 document.getElementById("beer").innerHTML ='<img src="images/bourbon.png">';
+
+	$('#restaurants').empty()
+	// $('#burger').empty(),
+	// $('#beer').empty();
+
+  });
+
+
+
 
 //Start of JQuery to get the JS to actually run ****************************************
 
 // $(function(){
 //   foodApp.init();
+
 // }); 
---
+++ pairs.js
@@ -0,0 +1,26 @@
+var pairs = [
+   {
+   		'id':1,
+		'name':'wine',
+		'bigImg':'images/bourbonwhiskey.png',
+		'match':1
+	},
+	
+	{'id':2,
+		'name':'steak',
+		'bigImg':'images/Ray1.jpg',
+		'match':'1'
+	},
+
+    {'id':3,
+		'name':'beer',
+		'bigImg':'images/USA.jpg',
+		'match':'2'
+	},
+
+	{'id':4,
+		'name':'burger',
+		'bigImg':'images/ducks1.jpg',
+		'match':'2'
+	}
+]
